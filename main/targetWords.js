276b9b5fa946a78df4cfdd54649b0c953c87b17c
Jamie Cartin <jamie-cartin@lambdastudents.com>
2022-10-21 16:02:09 -0400

    Create targetWords.js

---
+++ main/targetWords.js
@@ -0,0 +1,21 @@
+const targetWords = [
+  "rebut",
+  "humph",
+  "awake",
+  "blush",
+  "focal",
+  "evade",
+  "naval",
+  "serve",
+  "heath",
+  "dwarf",
+  "model",
+  "karma",
+  "stink",
+  "grade",
+  "quiet",
+  "bombs",
+  "smile"
+];
+
+export default targetWords;
